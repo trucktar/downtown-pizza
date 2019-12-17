ccade601d5d2e4ef94f11692123e950cf7394d35
Nyota Mwangi <nyota.benjamin@gmail.com>
2019-12-17 14:14:57 +0300

    Extend the PizzaOrder with the method getPrice()
    This method calculate the orderPrice for every pizza order that is
    placed.

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,20 @@ function PizzaOrder(pizzaSize, pizzaCrust, pizzaToppings, pizzaCount) {
   this.toppings = pizzaToppings; //toppings array
   this.count = pizzaCount;
 }
+
+PizzaOrder.prototype.getPrice = function() {
+  let sizePrice, crustPrice, toppingsPrice;
+  sizePrice = parseInt(this.size.val());
+  crustPrice = parseInt(this.crust.val());
+
+  toppingsPrice = this.toppings.map(function() {
+    return parseInt($(this).val());
+  });
+  let toppingsTotalPrice = 0;
+  for (let i = 0; i < toppingsPrice.length; i++) {
+    toppingsTotalPrice += toppingsPrice[i];
+  }
+
+  let orderPrice = (sizePrice + crustPrice + toppingsTotalPrice) * this.count;
+  return orderPrice;
+};
