5669942efb9c90f464f06ccd57b4738e928b16aa
Nyota Mwangi <nyota.benjamin@gmail.com>
2019-12-26 12:21:13 +0300

    Change menu on type and size change using updateMenu function

--- js/pizza-menu.js
+++ js/pizza-menu.js
@@ -1,45 +1,39 @@
 $(document).ready(function() {
-  // On page load, reset selected option
+  // Default to 1st <option> for all <select>s
   $("select").prop("selectedIndex", 0);
 
   let $type, $size, $name, $price;
-  $type = $("select#pizzaType :selected");
-  $size = $("select#pizzaSize :selected");
-
-  $("select#pizzaType").change(function() {
-    $type = $(":selected", this);
-    $("tbody").each(function() {
-      if ($(this).index() === $type.index() + 1) {
-        $(this).show();
-      } else {
-        $(this).hide();
-      }
-    });
-  });
-  $("select#pizzaSize").change(function() {
-    $size = $(":selected", this);
-    $("td").each(function() {
-      if ($(this).index() === $size.index() + 1) {
-        $(this).show();
-      } else {
-        $(this).hide();
-      }
-    });
+  $("select").click(function() {
+    if (this.id === "pizzaType") {
+      updateMenu(this, "tbody");
+    } else {
+      updateMenu(this, "td");
+    }
   });
 
   $("tr:not(:only-child)").click(function() {
+    $type = $("select#pizzaType :selected");
+    $size = $("select#pizzaSize :selected");
+
     $name = $("th", this);
     $price = $('td:not(":hidden")', this);
-    let newPizza = new PizzaOrder(
-      $type.html(),
-      $size.html(),
-      $name.html(),
-      $price.html()
-    );
+    let newPizza = new PizzaOrder($type.html(), $size.html(), $name.html(), $price.html());
 
     console.log(newPizza);
   });
 });
+
+function updateMenu(select, target) {
+  $(select).change(function() {
+    $(target).each(function() {
+      if ($(this).index() === select.selectedIndex + 1) {
+        $(this).fadeIn();
+      } else {
+        $(this).fadeOut();
+      }
+    });
+  });
+}
 
 function PizzaOrder(pizzaType, pizzaSize, pizzaName, pizzaPrice) {
   this.type = pizzaType;
